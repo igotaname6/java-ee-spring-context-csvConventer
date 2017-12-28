c19118fbc12c1432eb6b727708401ecffd591a7c
Szymon Wojtas <szymon.wojtas@gmail.com>
2017-12-28 19:35:55 +0100

    Implements main method

--- src/main/java/com/codecool/scc/ConventerApplication.java
+++ src/main/java/com/codecool/scc/ConventerApplication.java
@@ -4,5 +4,14 @@ public class ConventerApplication {
 
     public static void main(String[] args) {
 
+        if(args.length > 2){
+            System.out.println("To many arguments");
+            System.exit(1);
+        } else if(args.length == 0){
+            System.out.println("Csv file path to convert required");
+            System.exit(1);
+        } else if(args.lenght == 1){
+
+        }
     }
 }
