76093163b72012e7be8e46dc879d85bec0a212f2
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-01-02 13:24:48 +0100

    Implements createByFormat method in OutputFormatterFactory

--- src/main/java/com/codecool/scc/outputProcessing/OutputFormatterFactory.java
+++ src/main/java/com/codecool/scc/outputProcessing/OutputFormatterFactory.java
@@ -1,4 +1,19 @@
 package com.codecool.scc.outputProcessing;
 
+import com.codecool.scc.OutputFormat;
+
 public class OutputFormatterFactory {
+    
+    public OutputFormatter createByFormat(OutputFormat outputFormat){
+        OutputFormatter  outputFormatter = null;
+        
+        if(outputFormat  == OutputFormat.JSON){
+            outputFormatter = new JsonOutputFormatter();
+        } else if(outputFormat == OutputFormat.XML){
+            outputFormatter = new XmlOutputFormatter();
+        } else if(outputFormat == OutputFormat.TABLE){
+            outputFormatter = new XmlOutputFormatter();
+        }
+        return outputFormatter;
+    }
 }
