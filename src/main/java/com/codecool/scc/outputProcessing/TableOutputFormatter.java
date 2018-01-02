0f5132b8ae4f9d1de9b514b6867290c356b1499a
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-01-02 13:02:28 +0100

    Create TableOutputFormatter implementation of OutputFormatter interface

---
+++ src/main/java/com/codecool/scc/outputProcessing/TableOutputFormatter.java
@@ -0,0 +1,13 @@
+package com.codecool.scc.outputProcessing;
+
+import com.codecool.scc.outputProcessing.OutputFormatter;
+
+import java.util.List;
+
+public class TableOutputFormatter implements OutputFormatter {
+    
+    @Override
+    public void printToConsole(List<String> data) {
+    
+    }
+}
