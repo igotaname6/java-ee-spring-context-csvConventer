73f6931c8f3f6128c5862d06ff74687a58aef877
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-01-02 13:00:14 +0100

    Create JsonOutputFormatter implementation of OutputFormatter interface

---
+++ src/main/java/com/codecool/scc/outputProcessing/JsonOutputFormatter.java
@@ -0,0 +1,11 @@
+package com.codecool.scc.outputProcessing;
+
+import java.util.List;
+
+public class JsonOutputFormatter implements OutputFormatter {
+
+    @Override
+    public void printToConsole(List<String> data) {
+
+    }
+}
