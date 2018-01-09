df229e5f6197d8b4e94a9b969f341fd3f510f9c8
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-01-09 15:39:40 +0100

    Implement printToConsole method in JsonOutputFormatter

--- src/main/java/com/codecool/scc/outputProcessing/JsonOutputFormatter.java
+++ src/main/java/com/codecool/scc/outputProcessing/JsonOutputFormatter.java
@@ -1,11 +1,26 @@
 package com.codecool.scc.outputProcessing;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class JsonOutputFormatter implements OutputFormatter {
 
     @Override
-    public void printToConsole(List<String> data) {
-
+    public void printToConsole(List<Map<String, String>> fileData) {
+    
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    
+        try {
+            mapper.writeValue(System.out, fileData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
