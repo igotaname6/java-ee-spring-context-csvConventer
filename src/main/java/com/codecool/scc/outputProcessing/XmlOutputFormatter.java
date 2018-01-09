5b4654d626fcad63b42eec7225664a0d4616da4d
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-01-09 16:33:46 +0100

    Implements printToConsole method

--- src/main/java/com/codecool/scc/outputProcessing/XmlOutputFormatter.java
+++ src/main/java/com/codecool/scc/outputProcessing/XmlOutputFormatter.java
@@ -1,11 +1,22 @@
 package com.codecool.scc.outputProcessing;
 
 import java.util.List;
+import java.util.Map;
 
 public class XmlOutputFormatter implements OutputFormatter {
     
-    @Override
-    public void printToConsole(List<String> data) {
     
+    @Override
+    public void printToConsole(List<Map<String, String>> file) {
+    
+        int id = 1;
+        for(Map<String, String> lines : file){
+                System.out.format("<%d>\n", id);
+            for (String record : lines .keySet()){
+                System.out.format("\t<%1$s>%2$s></%1$s>\n", record, lines .get(record));
+            }
+            System.out.format("</%d>\n", id);
+            id++;
+        }
     }
 }
