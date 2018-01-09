bed027071d059402389f56a2d90cca5cef201e8f
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-01-09 16:32:44 +0100

    Implements convert methods

--- src/main/java/com/codecool/scc/conventers/SimpleCsvConventer.java
+++ src/main/java/com/codecool/scc/conventers/SimpleCsvConventer.java
@@ -1,21 +1,34 @@
 package com.codecool.scc.conventers;
 
 import com.codecool.scc.OutputFormat;
+import com.codecool.scc.outputProcessing.OutputFormatter;
+import com.codecool.scc.outputProcessing.OutputFormatterFactory;
 
-import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class SimpleCsvConventer {
-    private OutputFormat outputFormat;
 
-    void convert(File file, OutputFormat outputFormat){
+    private OutputFormat outputFormat;
+    private DataProvider dataProvider;
+
+    public SimpleCsvConventer(DataProvider dataProvider){
+        //Default value of outputFormat use if no type is given in convert method.
+        this.outputFormat = OutputFormat.TABLE;
+        this.dataProvider = dataProvider;
+    }
+
+    public void convert(String fileUrl, OutputFormat outputFormat) throws IOException {
         setOutputFormat(outputFormat);
-        System.out.println("I convert CSV to " + outputFormat.getOutputName());
+        this.convert(fileUrl);
 
     }
 
-    void convert(File file){
-        setOutputFormat(OutputFormat.TABLE);
-        System.out.println("I convert CSV to " + outputFormat.getOutputName());
+    public void convert(String fileUrl) throws IOException {
+        OutputFormatter outputFormatter = new OutputFormatterFactory().createByFormat(this.outputFormat);
+        List<Map<String, String>> dataToConvert = dataProvider.readData(fileUrl);
+        outputFormatter.printToConsole(dataToConvert);
     }
 
     public OutputFormat getOutputFormat() {
