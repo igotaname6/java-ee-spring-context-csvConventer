9624b9eed7258c09dc8bd363c3a6ec55e4f90266
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-01-09 16:31:38 +0100

    Implements getOutputFromString method

--- src/main/java/com/codecool/scc/OutputFormat.java
+++ src/main/java/com/codecool/scc/OutputFormat.java
@@ -1,5 +1,7 @@
 package com.codecool.scc;
 
+import com.codecool.scc.outputProcessing.InvalidOutputTypeException;
+
 public enum OutputFormat {
     TABLE("table"),
     JSON("json"),
@@ -13,5 +15,17 @@ public enum OutputFormat {
 
     public String getOutputName() {
         return outputName;
+    }
+    
+    public static OutputFormat getOutputFormatFromString(String string){
+        if(string.toLowerCase().equals("table")){
+            return TABLE;
+        } else if(string.toLowerCase().equals("json")){
+            return JSON;
+        }else if(string.toLowerCase().equals("xml")){
+            return XML;
+        } else {
+            throw new InvalidOutputTypeException(string);
+        }
     }
 }
