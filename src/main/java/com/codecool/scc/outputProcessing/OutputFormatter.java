dd09b4e17a5c6b9a02979b019bce865a7a1bc549
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-01-09 11:04:35 +0100

    change method printToConsole definition - now it takes List<Map<String, String>> as parameter

--- src/main/java/com/codecool/scc/outputProcessing/OutputFormatter.java
+++ src/main/java/com/codecool/scc/outputProcessing/OutputFormatter.java
@@ -1,8 +1,9 @@
 package com.codecool.scc.outputProcessing;
 
 import java.util.List;
+import java.util.Map;
 
 public interface OutputFormatter {
 
-    void printToConsole(List<String> data);
+    void printToConsole(List<Map<String, String>> File);
 }
