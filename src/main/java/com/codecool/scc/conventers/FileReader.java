b11e5750d451062776ab69e539760c5795803cbf
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-01-09 10:56:45 +0100

    Implemented new body of readData method

--- src/main/java/com/codecool/scc/conventers/FileReader.java
+++ src/main/java/com/codecool/scc/conventers/FileReader.java
@@ -1,17 +1,32 @@
 package com.codecool.scc.conventers;
 
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class FileReader implements DataProvider{
-
-    public List<String> readData(String fileUrl) throws IOException {
-
-        java.io.FileReader ioFileReader = new java.io.FileReader(fileUrl);
-        BufferedReader bufferedReader = new BufferedReader(ioFileReader);
-
-        return bufferedReader.lines().collect(Collectors.toList());
-
+public class FileReader implements DataProvider {
+    
+    public List<Map<String, String>> readData(String fileUrl) throws IOException {
+    
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(fileUrl))) {
+            List<Map<String, String>> dataMap = new ArrayList<>();
+            String[] headerLine = bufferedReader.readLine().split(",");
+            
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                String[] splitedLine = line.split(",");
+                Map<String, String> lineMap = IntStream.range(0, splitedLine.length)
+                                                .boxed()
+                                                .collect(Collectors.toMap(i -> headerLine[i], i -> splitedLine[i]));
+                dataMap.add(lineMap);
+            }
+            return dataMap;
+        }
     }
 }
+
